0f6a0bb33e461ddf1c4d6369bd0e9f50c5d5dc04
arafique <arafique@MCBA202-C20.classrooms.stlcc>
2017-09-20 11:01:02 -0500

    first commit

---
+++ src/java1githubdemo/Java1GitHubDemo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java1githubdemo;
+
+/**
+ *
+ * @author arafique
+ */
+public class Java1GitHubDemo {
+    //psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hello, world!");
+    
+    }
+    
+}
